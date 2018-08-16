1ecc6f59d693c08efc4110d5690b22b2a0c0cc61
TowerLeon <top770977@163.com>
2018-08-16 18:57:13 +0800

    20180816 1857

--- src/HOW2J.java
+++ src/HOW2J.java
@@ -648,3 +648,44 @@ class try1{
 
 //类和对象-练习-单例模式
 //懒汉与饿汉，略
+
+
+//类和对象-练习-枚举
+class MeiJu{
+    enum Herotype{
+        TANK,WIZARD,ASSASSIN,ASSIST,WARRIOR,RANGED,PUSH,FARMING
+    }
+
+    public static void main(String[] args) {
+        for (Herotype x: Herotype.values()
+             ) switch (x){
+            case TANK:
+                System.out.println("坦克");
+                break;
+            case WIZARD:
+                System.out.println("法师");
+                break;
+            case ASSASSIN:
+                System.out.println("刺客");
+                break;
+            case WARRIOR:
+                System.out.println("近战");
+                break;
+            case ASSIST:
+                System.out.println("辅助");
+                break;
+            case RANGED:
+                System.out.println("远程");
+                break;
+            case PUSH:
+                System.out.println("推进");
+                break;
+            case FARMING:
+                System.out.println("打野");
+                break;
+                default:
+                    break;
+        }
+
+        }
+    }
